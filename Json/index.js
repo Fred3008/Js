f27b6fc8ec5b0ba3377f8d505f53f4a3883fb778
Frédéric Lopes <fred.fadiez@gmail.com>
2021-12-20 13:12:23 +0100

    json

---
+++ Json/index.js
@@ -0,0 +1,36 @@
+let google = {
+
+    "siegeSocial": "Googleplex, Mountain View, California, United States",
+    "fondateurs": [{
+            "name": "Larry Page",
+            "birth": "26/03/1973",
+            "city": "Michigan"
+        },
+        {
+            "name": "Sergey Brin",
+            "birth": " 21/08/1973",
+            "city": "Moscou"
+        }
+    ],
+    "transactions": [{
+            "year": 2008,
+            "valeur": 16.49
+        },
+        { "year": 2012, "valeur": 37.97 },
+        { "year": 2016, "valeur": 89.46 },
+        { "year": 2018, "valeur": 136.2 }
+    ]
+
+
+}
+
+for (let name in google) {
+    console.log(name);
+
+}
+let clee = Object.keys(google)
+    //console.log(clee);
+
+for (i = 0; i < clee.length; i++) {
+    console.log(google[clee[i]]);
+}
